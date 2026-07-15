5867c73436fce662ca11cff1463a6549faa7f35a
zephyyyrus <nasifforpc@gmail.com>
2026-07-16 00:33:51 +0530

    Add files via upload
    project of electricity bill calculator

---
+++ electricity_billcalculator.py
@@ -0,0 +1,18 @@
+unit=float(input("Enter your unit: "))
+
+
+if(unit<=100):
+  bill=unit*1.5
+elif(unit>100 and unit<=200):
+  bill=(unit - 100)*2.5 + 100*1.5
+elif(unit>200 and unit<=300):
+  bill=(unit-200)*4 + 100*4
+else :
+  bill=(unit-300)*6 +100*8
+
+
+if (bill>1000):
+  bill=bill-bill*0.05
+
+print("your total bill is ",bill)
+print("your total consumed unit is ", unit)
